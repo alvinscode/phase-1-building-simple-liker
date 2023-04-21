50b4f1a98bdc2b6116483458385a94aa6cfe7ccc
Alvin Li <aflip24@gmail.com>
2023-04-21 07:23:33 -0500

    finished lab

--- main.js
+++ main.js
@@ -4,8 +4,33 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const heartGlyphs = document.querySelectorAll('.like-glyph');
+const errorModal = document.getElementById('modal');
+const errorMessage = document.getElementById('modal-message');
 
-
+heartGlyphs.forEach(heartGlyph => {
+  heartGlyph.addEventListener('click', () => {
+    mimicServerCall()
+      .then(response => {
+        console.log(response);
+        if (heartGlyph.innerText === EMPTY_HEART) {
+          heartGlyph.innerText = FULL_HEART;
+          heartGlyph.classList.add('activated-heart');
+        } else {
+          heartGlyph.innerText = EMPTY_HEART;
+          heartGlyph.classList.remove('activated-heart');
+        }
+      })
+      .catch(error => {
+        console.error(error);
+        errorMessage.innerText = error;
+        errorModal.classList.remove('hidden');
+        setTimeout(() => {
+          errorModal.classList.add('hidden');
+        }, 3000);
+      });
+  });
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
